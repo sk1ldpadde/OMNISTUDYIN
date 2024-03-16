065534d19d58294bcebbc5b021ec09d9f3155dac
csknkdhbw <inf22111@lehre.dhbw-stuttgart.de>
2024-03-16 09:19:54 +0100

    Added friendship relationship between students
    little code fix
    
    Added friendship relationship between students

--- data_logic/models.py
+++ data_logic/models.py
@@ -1,34 +1,64 @@
-from neomodel import (config, StructuredNode, StringProperty, 
-                      IntegerProperty, UniqueIdProperty, RelationshipTo,
-                      EmailProperty, DateTimeFormatProperty)
+from neomodel import *
+
+import base64
 
 
 # Create your models here.
 
 
-# student model
+# Student model
 class Student(StructuredNode):
-    # student related information
-    student_id = UniqueIdProperty() # not needed because neomodel creates and id in the back itself
+    # Student related information
     forename   = StringProperty(required=True)
     surname    = StringProperty()
     
-    # date of birth
+    # Date of birth
     dob        = DateTimeFormatProperty(format="%d-%m-%Y")
 
+    # Use email as unique identifier
     email      = EmailProperty(required=True)
     password   = StringProperty() # stored as salted argon2i secret hash
     salt       = StringProperty()
 
-    # personal text
+    # Personal text
     bio        = StringProperty()
 
-    # university information
+    # University information
     uni_name   = StringProperty()
     degree     = StringProperty()
     semester   = IntegerProperty()
+    
+    # Profile picture - neomodel does not support file fields (blobs) 
+    # so we store it as a base64 string
+    profile_picture = StringProperty() 
+    
+    # Relationships
+    # Bidirectional friendship relationship
+    friends = RelationshipTo('Student', 
+                             'FRIEND', 
+                             model='FriendRel', 
+                             cardinality=ZeroOrMore)
+    
+    def delete(self):
+        # Assign unidirectional relationship for all friends
+        for rel in self.friends.all():
+            rel.set_bidrectional(False)
+        # Delete the node
+        super().delete()
 
     def __str__(self):
         return (self.student_id, self.email)
+
+
+# Friend relationship model
+class FriendRel(StructuredNode):
+    # When was the friendship created
+    since = DateTimeFormatProperty(format="%d-%m-%Y",
+                                   default_now=True)
     
+    # Is the friendship bidirectional
+    bidrectional = BooleanProperty(default=False)
     
+    def set_bidrectional(self, value):
+        self.bidrectional = value
+        self.save()    
