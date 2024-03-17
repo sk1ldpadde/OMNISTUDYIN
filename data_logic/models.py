a99e57201173bb9f3304855e6e12ca6ddfbcb219
sk1ldpadde <artisan-active0w@icloud.com>
2024-03-17 10:54:04 +0100

    Added Ad_group & Ad models
    - Added Ad_Group Model which has ads as relationships and an admin
    - Added Ad Model which has an admin
    
    - fixed a typo
    -reformated code through the autopep8 extention of vscode

--- data_logic/models.py
+++ data_logic/models.py
@@ -9,44 +9,44 @@ class FriendRel(StructuredRel):
     # When was the friendship created
     since = DateTimeFormatProperty(format="%d-%m-%Y",
                                    default_now=True)
-    
+
     # Is the friendship bidirectional
-    bidrectional = BooleanProperty(default=False)
-    
+    bidirectional = BooleanProperty(default=False)
+
     def set_bidrectional(self, value):
         self.bidrectional = value
-        self.save()    
+        self.save()
 
 
 # Student model
 class Student(StructuredNode):
     # Student related information
-    forename   = StringProperty(required=True)
-    surname    = StringProperty()
-    
+    forename = StringProperty(required=True)
+    surname = StringProperty()
+
     # Date of birth
-    dob        = DateTimeFormatProperty(format="%d-%m-%Y")
+    dob = DateTimeFormatProperty(format="%d-%m-%Y")
 
     # Use email as unique identifier
-    email      = EmailProperty(required=True)
-    password   = StringProperty() # stored as salted argon2i secret hash
+    email = EmailProperty(required=True)
+    password = StringProperty()  # stored as salted argon2i secret hash
 
     # Personal text
-    bio        = StringProperty()
+    bio = StringProperty()
 
     # University information
-    uni_name   = StringProperty()
-    degree     = StringProperty()
-    semester   = IntegerProperty()
-    
-    # Profile picture - neomodel does not support file fields (blobs) 
+    uni_name = StringProperty()
+    degree = StringProperty()
+    semester = IntegerProperty()
+
+    # Profile picture - neomodel does not support file fields (blobs)
     # so we store it as a base64 string
     profile_picture = StringProperty()
-    
+
     # Additional information
     # TODO Use Google Geolocation API to get the location from the zip code
-    zip_code   = StringProperty()
-    
+    zip_code = StringProperty()
+
     """
     Interests and goals are stored as JSON objects
     
@@ -56,19 +56,25 @@ class Student(StructuredNode):
         "goals": ["Learn more about Django", "Get better at Python"]
     }
     """
-    interests_and_goals  = JSONProperty()
-    
+    interests_and_goals = JSONProperty()
+
     # --------------------------
     # END OF STUDENT INFORMATION
     # --------------------------
-    
+
     # Relationships
     # Bidirectional friendship relationship
-    friends = RelationshipTo('Student', 
-                             'FRIEND', 
-                             model=FriendRel, 
+    friends = RelationshipTo('Student',
+                             'FRIEND',
+                             model=FriendRel,
                              cardinality=ZeroOrMore)
-    
+
+    # TODO: please review if the following relationships are correct
+    creator_of_ad_group = RelationshipTo(
+        'Ad_Group', 'ADMIN', cardinality=ZeroOrMore)
+
+    creator_of_ad = RelationshipTo('Ad', 'ADMIN', cardinality=ZeroOrMore)
+
     def delete(self):
         # Assign unidirectional relationship for all friends
         for rel in self.friends.all():
@@ -77,4 +83,57 @@ class Student(StructuredNode):
         super().delete()
 
     def __str__(self):
-        return (self.student_id, self.email)
+        return f"{self.forename} {self.surname} ({self.email})"
+
+
+# Ad_group model: Ad group is a collection of ads kind of like a "subreddit"
+
+    class Ad_Group(StructuredNode):
+        # Ad group information
+        name = StringProperty()
+        description = StringProperty()
+
+    # TODO: please review if the following relationships are correct
+        # Relationships
+        # Ad group has many ads
+        ads = RelationshipTo('Ad', 'AD_IN', cardinality=ZeroOrMore)
+
+        # Ad group is created by a student --> Admin of the ad group
+        # cardinality is ZeroOrMore because a student can create multiple ad groups,
+        # but we also want to have the possibility to have "standard groups" which are created by the system
+        admin = RelationshipFrom('Student', 'ADMIN', cardinality=ZeroOrMore)
+
+        def __str__(self):
+            return self.name
+
+        def delete(self):
+            # Delete all ads in the group
+            for ad in self.ads.all():
+                ad.delete()
+            # Delete the ad group
+            super().delete()
+
+
+class Ad(StructuredNode):
+    # Ad information
+    title = StringProperty()
+    description = StringProperty()
+    # Image is stored as a base64 string
+    image = StringProperty()
+
+    # TODO: please review if the following relationships are correct
+
+    # Relationships
+    # Ad belongs to an ad group
+    ad_group = RelationshipFrom('Ad_Group', 'AD_IN', cardinality=One)
+
+    # Ad is created by a student --> Admin of the ad
+    # cardinality is ZeroOrMore because of the possible implementation of "standard ads"
+    admin = RelationshipFrom('Student', 'ADMIN', cardinality=ZeroOrMore)
+
+    def __str__(self):
+        return self.title
+
+    def delete(self):
+        # Delete the ad
+        super().delete()
